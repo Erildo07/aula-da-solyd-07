0e004fb38ac814d5769bff08ca0c9e33596ae94f
Erildo07 <erildonunes12@gmail.com>
2025-04-19 21:15:34 -0400

    aula da solyd

--- aula10-solyd/aula10.py
+++ aula10-solyd/aula10.py
@@ -1,4 +1,14 @@
-arquivo = open("arquivo.txt","w")
+#arquivo = open("/home/kali/Downloads/𝐭𝐨𝐝𝐨𝐫𝐨𝐤𝐢 𝐬𝐡𝐨𝐮𝐭𝐨 _ 𝐛𝐨𝐤𝐮 𝐧𝐨 𝐡𝐞𝐫𝐨 𝐚𝐜𝐚𝐝𝐞𝐦𝐢𝐚.jpeg", "rb")
+arquivo = open("arquivo.txt", "w")
+# cria o arquivo txt rescreve
 arquivo.write("Fala comigo !!")
+# escreve na pasta aruivo.txt
 type(print(arquivo))
-print(arquivo)
+for i in range(0, 100):
+    arquivo.write("aa: "+str(i)+"\n")
+#print(arquivo)
+# "r" le o arquivo e printa  na terminal 
+#print(arquivo.read())
+
+#for linha in arquivo:
+#    print(linha)
