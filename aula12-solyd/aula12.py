30b7513f68bf73bc2682224c54bda96ebe0f3c0a
Erildo07 <erildonunes12@gmail.com>
2025-04-21 22:42:40 -0400

    aula da solyd

--- aula12-solyd/aula12.py
+++ aula12-solyd/aula12.py
@@ -1,10 +1,21 @@
 import requests    #Beautiful soup 4 BS$ pip install bs4
 
+cabecalho = {"user-agent ": "windows ", "Referer": "https:// google.com"}
+
+meus_cookies = {"ultima visita":"21-04-2025" }
+
+dados = {"usarname": "Eridlo",
+        "password": "123"}
+
 texto = None
 try:
-    requisicão =  requests.get("https://putsreq.com/6w8pVbaAOHY8aSq2NLec")
+    #requisicão =  requests.get("https://putsreq.com/b1Gv9Fy4IAZgp3a8qc1I")
+    requisicão =  requests.post("https://putsreq.com/b1Gv9Fy4IAZgp3a8qc1I",
+                                headers=cabecalho,
+                                cookies=meus_cookies,
+                                data=dados)
     texto = requisicão.text
-    print(requisicão.text)
+    #print(requisicão.text)
 except Exception as e:
     print("Resquisição deu erro: ", e)
 
