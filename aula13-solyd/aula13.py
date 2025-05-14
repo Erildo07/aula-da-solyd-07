900adedb930932923063d70e7638e68233ddbeb2
Erildo07 <erildonunes12@gmail.com>
2025-05-14 19:30:02 -0400

    inf

--- aula13-solyd/aula13.py
+++ aula13-solyd/aula13.py
@@ -2,18 +2,44 @@ import requests
 import json
 req = None
 
-try:
-    req = requests.get('http://www.omdbapi.com/?t=Harry+potter&apikey=3b28a7e')
-except:
-    print("Erro na conexão")
-    exit()
+def requisição(titulo):
+    try:
+        req = requests.get(f'http://www.omdbapi.com/?t={titulo}&apikey=3b28a7e')
+        dicionario = json.loads(req.text)
+        return dicionario, req.url  
+    except:
+        print("Erro na conexão")
+        return None
 
-dicionario = json.loads(req.text)
+def printar_detahes(filme):
 
-print(dicionario["Title"])
-print("Linguagem:", dicionario["Language"])
-print("Gênero:", dicionario["Genre"])
-print("Tempo de execução:", dicionario["Runtime"])
-print("Ano:", dicionario["Year"])
-print("Atores:", dicionario["Actors"])
-print("Diretor:", dicionario["Director"])
+    print("Titulo:", filme["Title"])
+    print("Avaliação imdb:", filme["imdbRating"])
+    print("Linguagem:", filme["Language"])
+    print("Gênero:", filme["Genre"])
+    print("Tempo de execução:", filme["Runtime"])
+    print("Ano:", filme["Year"])
+    print("Atores:", filme["Actors"])
+    print("Trofeus:", filme["Awards"])
+    print("Diretor:", filme["Director"])
+    print("Poster:", filme["Poster"])
+    print("")
+
+sair = False
+while not sair:
+    print("")
+    print("         ------************--------")
+    print("")
+    op = input("Escreva o nome de um filme ou Sair para fechar: ")
+    print("")
+
+    if op.upper() == "SAIR":
+        sair = True
+        print("Saindo .....")
+    else:
+        filme, url = requisição(op)
+        if filme is None or filme.get("Response") == "False":
+            print("Filme não encontrado")
+        else:
+            printar_detahes(filme)
+            print("URL gerada:", url)
