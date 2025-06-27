9bbef5aaf50eea2a7bd6db813fdcb481aa7484a9
Erildo07 <erildonunes12@gmail.com>
2025-06-27 14:57:27 -0400

    code

--- aula15-solyd/aula15.py
+++ aula15-solyd/aula15.py
@@ -1,14 +1,24 @@
 import requests
 import json
+import time
+import datetime
 
-# Solicita cotação de dólar, euro e bitcoin
-url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL"
-resposta = requests.get(url)
 
-cotação = json.loads(resposta.text)
+    # Solicita cotação de dólar, euro e bitcoin
+while True:
 
-# Mostra as cotações
-print("### COTAÇÃO ###")
-print("Dólar:", cotação['USDBRL']['bid'])
-print("Euro:", cotação['EURBRL']['bid'])
-print("Bitcoin:", cotação['BTCBRL']['bid'])
+    url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL"
+    resposta = requests.get(url)
+
+    cotação = json.loads(resposta.text)
+
+    # Mostra as cotações
+    print("### COTAÇÃO ###  ", datetime.datetime.now())
+    print("Dólar:", cotação['USDBRL']['bid'])
+    print("Euro:", cotação['EURBRL']['bid'])
+    print("Bitcoin:", cotação['BTCBRL']['bid'])
+    print("")
+    print("************")
+    print("")
+    time.sleep(5)
+    
